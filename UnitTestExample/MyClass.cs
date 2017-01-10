300bd58816de3df08b4f6c9b3cdc5d4ea1d594af
Farland <farlandchen@eslite.com>
2017-01-10 12:13:39 +0800

    Init Commit.

---
+++ UnitTestExample/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample
+{
+    public class MyClass
+    {
+        /// <summary>
+        /// 加總數值
+        /// </summary>
+        /// <param name="Num1">數值1</param>
+        /// <param name="Num2">數值2</param>
+        /// <returns></returns>
+        public int Sum(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        /// <summary>
+        /// 驗證字串中是否有特殊字元
+        /// </summary>
+        /// <param name="validString">驗證字串</param>
+        /// <returns></returns>
+        public bool hasSpecialCharacter(string validString)
+        {
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(@"^[^~!@#$%^&*()_?\s+]+$");
+            return !reg.IsMatch(validString);
+        }
+    }
+}
---
+++ UnitTestExampleTests/MyClassTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Tests
+{
+    [TestClass()]
+    public class MyClassTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            //3A Pattern
+
+            /* Arrange:
+             * 初始化環境
+             * 建立測試行為
+             * 設定預期結果
+             */
+            #region Arrange
+            var myClass = new MyClass();
+            //初始化變數
+            int a = 1;
+            int b = 2;
+            //預期數值
+            var expected = 3;
+            #endregion
+
+            /*
+             * Action: 執行測試
+             */
+            #region Action
+            //執行Sum方法
+            var actual = myClass.Sum(a, b);
+            #endregion
+
+            /*
+             * Assert: 驗證是否如預期結果
+             */
+            #region Assert
+            //驗證是否為空值，這裡其實可以不用做
+            Assert.IsNotNull(actual);
+            //驗證結果是否如預期
+            Assert.AreEqual(expected, actual);
+            #endregion
+        }
+
+        [TestMethod()]
+        public void hasSpecialCharacterTest()
+        {
+            #region Arrange
+            var myClass = new MyClass();
+            string s = "Hello, how are you?";
+            var expected = true;
+            #endregion
+
+            #region Action
+            var actual = myClass.hasSpecialCharacter(s);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(expected, actual);
+            #endregion
+        }
+    }
+}
